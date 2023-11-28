634028074c319f86a9e78e2bae339e0d23daef26
Noha Salah Eldeen <nohasalahforstudy@gmail.com>
2023-11-28 10:51:18 -0800

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
-    print("{:02d}".format(number), end="")
     if number == 99:
-        break
+        print("{}".format(number))
     else:
-        print(", ", end="")
+        print("{:02d}".format(number), end=", ")
