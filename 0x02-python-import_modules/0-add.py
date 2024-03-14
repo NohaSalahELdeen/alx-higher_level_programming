fad76b5129c18399f0e7ff8597c00dbe7969c365
Noha Salah <nohasalahforstudy@gmail.com>
2024-03-14 04:50:16 -0700

    add 1 + 2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,13 +2,10 @@
 if __name__ == "__main__":
     from add_0 import add
     """
-        import add function from add_0 file
-        that print the result of the addition 1 + 2 = 3
-
-        args:
-            a: first number
-            b: second number
+        import add function from py file
+        print the result of the addition 1 + 2 = 3
     """
+
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
