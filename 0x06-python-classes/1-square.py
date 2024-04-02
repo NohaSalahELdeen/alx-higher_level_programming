0864409e7a01176ce29439683180870d9114b976
Noha Salah <nohasalahforstudy@gmail.com>
2024-04-02 16:35:47 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" defines a square """
+
+
+class Square:
+    """ class that defines a square """
+
+    def __init__(self, size):
+        """ initailize Private instance attribute
+
+        Args:
+            size: The size of a square
+        """
+
+        self.__size = size
