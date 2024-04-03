9a9078881c6cbca599bfa1d4cddbc4aaf16a9f9a
Noha Salah <nohasalahforstudy@gmail.com>
2024-04-03 14:32:18 -0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Define a square class """
+
+
+class Square:
+    """  a class Square that defines a square """
+
+    def __init__(self, size=0):
+        """ defined a method
+
+        Args:
+            size: The size of a square
+        """
+        self.__size = size
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
