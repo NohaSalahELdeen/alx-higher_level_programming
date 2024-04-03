cd2c3cb3d8d89819dc1f2389c662aed184531c77
Noha Salah <nohasalahforstudy@gmail.com>
2024-04-03 08:12:52 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             counter += 1
         except IndexError:
-            continue
+            print("Error")
     print()
-    return conuter
+    return counter
