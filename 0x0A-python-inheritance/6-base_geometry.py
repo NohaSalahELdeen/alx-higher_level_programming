a77df15c79e2fcecdaf2af05c4ae38460e7f45a8
Noha Salah <nohasalahforstudy@gmail.com>
2024-04-21 03:25:44 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 6-base_geometry module """
+
+
+class BaseGeometry:
+    """ basegeometry class """
+
+    def area(self):
+        """ Public instance method """
+        raise Exception("area() is not implemented")
